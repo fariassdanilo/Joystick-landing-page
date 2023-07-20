b6a773a3d21332b383b75cf22d0cc7a9d426f195
fariassdaniloe <danilo.fariass@hotmail.com>
2023-07-20 12:07:12 -0300

    att20/07

--- funcoes.js
+++ funcoes.js
@@ -5,11 +5,10 @@ function slider (anything) {
 
 let menu = document.querySelector ('#menu-icon');
 let navbar = document.querySelector ('.navbar');
-
-menu.onclick = () => {
+menu.addEventListener ('click', () => {
     menu.classList.toggle ('bx-x');
-    navbar.classList.toggle ('.open');
-}
+    navbar.classList.toggle ('open');
+})
 
 
 function menuShow() {
